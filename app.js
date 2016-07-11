e11d3ccabc644c19d08d51f61c8a0e3d390cc04a
Yury Lunev <yurylunev@me.com>
2016-07-11 16:13:16 +0300

    first
    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var i = 0;
+document.getElementsByName('addAchievement')[0].onclick = clickAchievement;
+
+function clickAchievement() {
+  i++;
+  var options = {
+    className: 'achievement-blue',
+    number: i,
+    img: 'owl-blue.png'
+  };
+
+  if (i % 2) {
+    options.className = '';
+  }
+  if (i % 3) {
+    options.img = '';
+  }
+    addAchievement(options);
+}
+
+function addAchievement(options) {
+  var className = options.className || '';
+  var number = options.number || '';
+  var img = options.img || '';
+
+  document.getElementById('achievements-area').innerHTML += '<div class="achievement ' + className + '">' +
+    '<div class="achievement-count-border"></div>' +
+    '<div class="achievement-background"><img src="' + img + '"/></div>' +
+    '<div class="achievement-count-area"><div class="achievement-count-number">' +
+    '' + number + '</div></div>' +
+    '</div>';
+}
